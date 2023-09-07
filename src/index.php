6e7585250a5f44774070dcd5b4cf518ba47257ca
Bosun18 <beliy-81@mail.ru>
2023-09-07 10:41:45 +0300

    created Makefile

--- src/index.php
+++ src/index.php
@@ -1,2 +1,21 @@
-<?
+<?php
 
+namespace Index;
+
+use Docopt;
+
+function run()
+{
+    $doc = <<<DOC
+    Generate diff
+
+    Usage:
+      gendiff (-h | --help)
+      gendiff (--version)
+
+    Options:
+      -h --help     Show this screen
+      --version     Show version
+    DOC;
+    echo $doc . PHP_EOL;
+}
