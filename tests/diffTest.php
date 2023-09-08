6718254282ac3c8e9922c58df1e14ad05441743e
Bosun18 <110723359+Bosun18@users.noreply.github.com>
2023-09-08 16:30:17 +0300

    update gendiff and diffTest

--- tests/diffTest.php
+++ tests/diffTest.php
@@ -1,3 +1,20 @@
 <?php
 
 namespace DiffTest;
+
+use PHPUnit\Framework\TestCase;
+
+use function Differ\genDiff;
+
+class DiffTest extends TestCase
+{
+    public function testGenDiff()
+    {
+        $resultJson = file_get_contents(__DIR__ . "/fixtures/resultJson.txt");
+        $file1 = __DIR__ . "/fixtures/file1.json";
+        $file2 = __DIR__ . "/fixtures/file2.json";
+        $this->assertEquals($resultJson, genDiff($file1, $file2));
+
+        echo "\n\033[42mFlat Tests passed!\033[0m\n";
+    }
+}
