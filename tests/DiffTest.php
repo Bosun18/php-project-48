602349f06f9d2943a0a63de18b4f9aeca24dce1d
Bosun18 <110723359+Bosun18@users.noreply.github.com>
2023-09-12 16:43:29 +0300

    update DiffTest

--- tests/DiffTest.php
+++ tests/DiffTest.php
@@ -10,9 +10,20 @@ class DiffTest extends TestCase
 {
     public function testGenDiff()
     {
-        $resultJson = file_get_contents(__DIR__ . "/fixtures/resultJson.txt");
-        $file1 = __DIR__ . "/fixtures/file1.json";
-        $file2 = __DIR__ . "/fixtures/file2.json";
-        $this->assertEquals($resultJson, genDiff($file1, $file2));
+//        $resultJson = file_get_contents(__DIR__ . "/fixtures/resultJson.txt");
+//        $file1 = __DIR__ . "/fixtures/file1.json";
+//        $file2 = __DIR__ . "/fixtures/file2.json";
+//        $this->assertEquals($resultJson, genDiff($file1, $file2));
+
+        $file1 = $this->getPathToFixture('file1.json');
+        $file2 = $this->getPathToFixture('file2.json');
+        $expected = file_get_contents($this->getPathToFixture('resultJson.txt'));
+        $this->assertEquals($expected, genDiff($file1, $file2));
+    }
+
+    private function getPathToFixture($fixtureName)
+    {
+        return __DIR__ . "/fixtures/" . $fixtureName;
     }
 }
+
