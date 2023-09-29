b4a05d2729ac95f83e4c5d6943e33adb4d3f29e1
Bosun <beliy-81@mail.ru>
2023-09-29 11:42:14 +0300

    fix plain

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -2,7 +2,7 @@
 
 namespace Differ\Formatters\Plain;
 
-function normalize(mixed $value): string|int|float|bool
+function normalize(mixed $value): string|int|float
 {
     if (!is_array($value)) {
         return match ($value) {
