f07b7c7ad52f8b764d3b11a2a79143a5653a8c27
Bosun <beliy-81@mail.ru>
2023-10-04 09:44:19 +0300

    fix differ

--- src/Differ.php
+++ src/Differ.php
@@ -13,39 +13,41 @@ function buildTree(mixed $data1, mixed $data2): array
 
     return array_map(
         function ($key) use ($data1, $data2) {
+            $value = $data1[$key];
+            $value2 = $data2[$key];
             if (
                 array_key_exists($key, $data1) && array_key_exists($key, $data2)
-                && is_array($data1[$key]) && is_array($data2[$key])
+                && is_array($value) && is_array($value2)
             ) {
                 return [
                     'key' => $key,
                     'type' => 'nested',
-                    'value' => buildTree($data1[$key], $data2[$key]),
+                    'value' => buildTree($value, $value2),
                 ];
             } elseif (!array_key_exists($key, $data2)) {
                 return [
                     'key' => $key,
                     'type' => 'deleted',
-                    'value' => $data1[$key],
+                    'value' => $value,
                 ];
             } elseif (!array_key_exists($key, $data1)) {
                 return  [
                     'key' => $key,
                     'type' => 'added',
-                    'value' => $data2[$key],
+                    'value' => $value2,
                 ];
-            } elseif ($data1[$key] !== $data2[$key]) {
+            } elseif ($value !== $value2) {
                 return  [
                     'key' => $key,
                     'type' => 'updated',
-                    'value' => $data1[$key],
-                    'value2' => $data2[$key],
+                    'value' => $value,
+                    'value2' => $value2,
                 ];
             } else {
                 return [
                     'key' => $key,
                     'type' => 'immutable',
-                    'value' => $data1[$key]
+                    'value' => $value
                 ];
             }
         },
